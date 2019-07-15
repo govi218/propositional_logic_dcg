35af606320599bd49d69b6aebebdde68abdf2168
Govind Mohan <govi218mu@gmail.com>
2019-07-15 12:37:40 -0400

    Create prop.pl

---
+++ prop.pl
@@ -0,0 +1,15 @@
+prop  -->  p.
+prop  -->  q.
+prop  -->  r.
+prop  --> not,prop.
+prop  --> prop,and,prop.
+prop  --> prop,or,prop.
+prop  --> prop,implies,prop.
+
+not --> [not].
+and --> [and].
+or --> [or].
+implies --> [implies].
+p --> [p].
+q --> [q].
+r --> [r].
