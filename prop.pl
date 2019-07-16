2731f05d46ce0a772feae069b883ee31dabddf4b
Govind Mohan <govi218mu@gmail.com>
2019-07-15 22:08:46 -0400

    simplified rules

--- prop.pl
+++ prop.pl
@@ -1,29 +1,22 @@
 prop --> simple_prop.
 
 prop  --> not,simple_prop.
-prop  --> simple_prop,and,simple_prop.
-prop  --> simple_prop,or,simple_prop.
-prop  --> simple_prop,implies,simple_prop.
+prop  --> simple_prop,binary_op,simple_prop.
+prop  --> simple_prop,binary_op,complex_prop.
+prop  --> complex_prop,binary_op,simple_prop.
 
-prop  --> simple_prop,and,term.
-prop  --> term,and,simple_prop.
-
-prop  --> simple_prop,or,term.
-prop  --> term,or,simple_prop.
-
-prop  --> simple_prop,implies,term.
-prop  --> term,implies,simple_prop.
-
-prop  --> not,term.
-prop  --> term,and,term.
-prop  --> term,or,term.
-prop  --> term,implies,term.
+prop  --> not,complex_prop.
+prop  --> complex_prop,binary_op,complex_prop.
 
 simple_prop --> p.
 simple_prop --> q.
 simple_prop --> r.
 
-term --> open_b,prop,close_b.
+binary_op --> and.
+binary_op --> or.
+binary_op --> implies.
+
+complex_prop --> open_b,prop,close_b.
 
 open_b --> ['('].
 close_b --> [')'].
