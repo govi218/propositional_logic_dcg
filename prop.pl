85ef0869fccfb0130add399cc6108ee578e5df3f
Govind Mohan <govi218mu@gmail.com>
2019-07-15 20:06:57 -0400

    Update prop.pl

--- prop.pl
+++ prop.pl
@@ -1,11 +1,32 @@
-prop  -->  p.
-prop  -->  q.
-prop  -->  r.
-prop  --> not,prop.
-prop  --> prop,and,prop.
-prop  --> prop,or,prop.
-prop  --> prop,implies,prop.
+prop --> simple_prop.
 
+prop  --> not,simple_prop.
+prop  --> simple_prop,and,simple_prop.
+prop  --> simple_prop,or,simple_prop.
+prop  --> simple_prop,implies,simple_prop.
+
+prop  --> simple_prop,and,term.
+prop  --> term,and,simple_prop.
+
+prop  --> simple_prop,or,term.
+prop  --> term,or,simple_prop.
+
+prop  --> simple_prop,implies,term.
+prop  --> term,implies,simple_prop.
+
+prop  --> not,term.
+prop  --> term,and,term.
+prop  --> term,or,term.
+prop  --> term,implies,term.
+
+simple_prop --> p.
+simple_prop --> q.
+simple_prop --> r.
+
+term --> open_b,prop,close_b.
+
+open_b --> ['('].
+close_b --> [')'].
 not --> [not].
 and --> [and].
 or --> [or].
